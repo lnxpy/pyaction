ecbca71acbdeda76d06fa64fbfa517648ccfd754
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-07-29 01:19:09 +0330

    using rich for showing the instructions

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,30 @@
 import os
 import shutil
 
+from rich.console import Console
+from rich.markdown import Markdown
+
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+PROJECT_SLUG = "{{ cookiecutter.project_slug }}"
+GITHUB_USER = "{{ cookiecutter.github_username }}"
+
+GIT_INSTRUCTIONS_INFO = f"""
+- Now you can start working on it:
+
+```sh
+$ cd {PROJECT_SLUG} && git init
+```
+
+- Upload initial code to GitHub:
+
+```sh
+$ git add .
+$ git commit -m "Initial commit"
+$ git branch -M main
+$ git remote add origin https://github.com/{GITHUB_USER}/{PROJECT_SLUG}.git
+$ git push -u origin main
+"""
 
 
 def remove_file(filepath: str) -> None:
@@ -29,3 +52,9 @@ if __name__ == "__main__":
 
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
+
+    console = Console()
+
+    # project created successfuly
+    console.rule(f"[bold white]{PROJECT_SLUG} is created! :fire:")
+    console.print(Markdown(GIT_INSTRUCTIONS_INFO))
