b56c844c44e2161f979521e1b8e8447420a5712b
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-07-30 01:20:04 +0330

    installing the requirements first

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import os
 import shutil
-
-from rich.console import Console
-from rich.markdown import Markdown
+import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 PROJECT_SLUG = "{{ cookiecutter.project_slug }}"
+
+STARTING_VERSION = "{{ cookiecutter.version }}"
+
 GITHUB_USER = "{{ cookiecutter.github_username }}"
+
+CC_REQUIREMENTS_FILE = "cc_requirements.txt"
 
 GIT_INSTRUCTIONS_INFO = f"""
 - Now you can start working on it:
@@ -22,9 +25,10 @@ $ cd {PROJECT_SLUG} && git init
 ```sh
 $ git add .
 $ git commit -m "Initial commit"
+$ git tag v{STARTING_VERSION}
 $ git branch -M main
 $ git remote add origin https://github.com/{GITHUB_USER}/{PROJECT_SLUG}.git
-$ git push -u origin main
+$ git push -u origin main --tags
 """
 
 
@@ -46,12 +50,32 @@ def remove_directory(path: str) -> None:
     shutil.rmtree(os.path.join(PROJECT_DIRECTORY, path))
 
 
+def install_requirements():
+    requirements_file = os.path.join(PROJECT_DIRECTORY, CC_REQUIREMENTS_FILE)
+    subprocess.check_call(
+        [
+            "pip",
+            "install",
+            "--disable-pip-version-check",
+            "--no-python-version-warning",
+            "-qUr",
+            requirements_file,
+        ]
+    )
+    remove_file(CC_REQUIREMENTS_FILE)
+
+
 if __name__ == "__main__":
     if "{{ cookiecutter.has_dependencies }}" == "n":
         remove_file("requirements.txt")
 
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
+
+    install_requirements()
+
+    from rich.console import Console
+    from rich.markdown import Markdown
 
     console = Console()
 
