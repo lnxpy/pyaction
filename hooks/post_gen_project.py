73212245ada33099ce53baf6ee9fead564cfce11
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-08-01 16:50:01 +0330

    utils bug fixed

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,9 +2,21 @@
 import os
 import shutil
 
-from hooks.utils.messages import FURTHER_INSTRUCTIONS, TEMPLATE_CREATION
-
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+TEMPLATE_CREATION = "\n\r{project_slug} is created successfully! \U0001F525"
+
+FURTHER_INSTRUCTIONS = """
+Now you can start working on your action:
+    $ cd {project_slug} && git init
+
+Make a commit and upload initial code to GitHub:
+    $ git add .
+    $ git commit -m "Initial commit"
+    $ git tag v{version}
+    $ git branch -M main
+    $ git remote add origin https://github.com/{github_username}/{project_slug}.git
+    $ git push -u origin main --tags"""
 
 
 def remove_file(filepath: str) -> None:
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
 import sys
 
-from hooks.utils.messages import MARKETPLACE_WARNING
+MARKETPLACE_WARNING = """
+If you want to publish your action in the GitHub marketplace:
+
+    - make sure {action_name} is unique. It cannot match an existing action, user or organization name.
+"""
 
 if __name__ == "__main__":
     if "{{ cookiecutter.publish_in_marketplace }}".lower() == "y":
