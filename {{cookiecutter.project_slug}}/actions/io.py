ae593ce0166185f07926959370fae0f2a6caedd3
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-08-05 10:20:27 +0330

    multiple values for GITHUB_OUTPUT issue fixed
    fixes #12

---
+++ {{cookiecutter.project_slug}}/actions/io.py
@@ -0,0 +1,24 @@
+import os
+from typing import Dict
+
+BUFFER_PATH = os.environ["GITHUB_OUTPUT"]
+
+
+def write_to_output(context: Dict[str, str]) -> None:
+    """writes the keys (as variables) and values (as values) to the output buffer
+
+    Args:
+        context: variables and values
+
+    Examples:
+        In your project, use this function like:
+
+        >>> write_to_output({"name": "John", ...})
+
+        ``name`` will be the variable name and ``John`` is its value.
+        In your workflow, access the variable(s) like ``${{ steps.<step-id>.outputs.name }}``.
+    """
+
+    with open(BUFFER_PATH, "a") as _buffer:
+        for var, val in context.items():
+            _buffer.write(f"{var}={val}\r\n")
--- {{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/main.py
@@ -1,21 +1,22 @@
-# import os
+import os
 import sys
+from typing import List
 
-# from github import io
+from actions import io
 
 
-def main(args: list[str]) -> None:
+def main(args: List[str]) -> None:
     """main function
 
     Args:
-        args (list[str]): STDIN arguments
+        args: STDIN arguments
     """
 
     # now you can access the inputs like:
     #    f"Hello {os.environ["INPUT_NAME"]}"
 
     # you can write to output like:
-    #   io.write_to_output(var, val)
+    #   io.write_to_output({var: val, ...})
 
     pass
 
