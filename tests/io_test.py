5d620a914fec1e20f91e9e837df3e934c2abaebf
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-05-15 19:41:37 +0330

    testing multi-line output

--- tests/io_test.py
+++ tests/io_test.py
@@ -4,6 +4,7 @@ import tempfile
 import pytest
 
 from pyaction import io
+from pyaction.consts import MULTILINE_OUTPUT
 from pyaction.exceptions import WorkflowParameterNotFound
 
 test_env_vars = [
@@ -12,6 +13,8 @@ test_env_vars = [
     ("INPUT_BAZ", "baz", "true"),
     ("INPUT_QUX", "qux", "2020.05.03"),
 ]
+
+test_stream = str(os.path.join(tempfile.tempdir, "GITHUB_OUTPUT_TEST.txt"))
 
 
 @pytest.mark.parametrize("var,name,val", test_env_vars)
@@ -36,8 +39,29 @@ test_env_var_context = [
 
 @pytest.mark.parametrize("context,expected", test_env_var_context)
 def test_write_to_stream(context, expected):
-    test_stream = os.path.join(tempfile.tempdir, "GITHUB_OUTPUT_TEST.txt")
-    io.write(context, str(test_stream))
+    io.write(context, test_stream)
+
+    with open(test_stream, "r+") as file:
+        content = file.read()
+
+    assert content == expected
+
+
+test_multi_line_env_var_context = [
+    (
+        {"message": "Hello\nworld"},
+        MULTILINE_OUTPUT.format(variable="message", value="Hello\nworld"),
+    ),
+    (
+        {"phrase": "How\nAre\nYou?!"},
+        MULTILINE_OUTPUT.format(variable="phrase", value="How\nAre\nYou?!"),
+    ),
+]
+
+
+@pytest.mark.parametrize("context,expected", test_multi_line_env_var_context)
+def test_write_multiline_to_stream(context, expected):
+    io.write(context, test_stream)
 
     with open(test_stream, "r+") as file:
         content = file.read()
