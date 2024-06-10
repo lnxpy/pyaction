325f72cf9556e9facd8857d39c753b42929d4f85
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-06-10 03:33:13 +0330

    adding the rich library as part of the table generation flow

--- pyaction/workflow/utils.py
+++ pyaction/workflow/utils.py
@@ -1,16 +1,16 @@
 from typing import Dict, List
 
-from rich.table import Table
-
 from pyaction.workflow.consts import LOCAL_TABLE_COLS
 
 
-def generate_param_table(columns: List[Dict[str, str]] = LOCAL_TABLE_COLS) -> Table:
+def generate_param_table(columns: List[Dict[str, str]] = LOCAL_TABLE_COLS):
     """generates an output table
 
     Returns:
         Table: output table
     """
+
+    from rich.table import Table
 
     table = Table(show_lines=True)
     for col in columns:
