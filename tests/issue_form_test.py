146432142730247956212445d71916e0bbacbc9a
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-08-04 15:50:49 +0330

    updated

--- tests/issue_form_test.py
+++ tests/issue_form_test.py
@@ -1,19 +1,28 @@
-from unittest.mock import MagicMock
-
-import pytest
-from github.Repository import Repository
+from unittest.mock import patch
 
 from pyaction.issues import IssueForm
+from pyaction.issues.rendering import IssueTemplate
 
 
-@pytest.fixture
-def mock_repository():
-    repo = MagicMock(spec=Repository)
-    repo.get_issue.return_value = MagicMock(body="Test issue body")
-    return repo
+@patch("requests.get")
+def test_render_with_token(mock_requests_get):
+    issue_body = {"body": "Mock issue body for testing"}
+    mock_requests_get.return_value.json.return_value = issue_body
 
-
-def test_issue_form_render(mock_repository):
-    issue_form = IssueForm(mock_repository, 123)
+    issue_form = IssueForm("testuser/testrepo", 123, "SOMETOKEN")
     rendered_issue = issue_form.render()
+
     assert isinstance(rendered_issue, dict)
+    assert rendered_issue == IssueTemplate(issue_body["body"]).to_dict()
+
+
+@patch("requests.get")
+def test_render_without_token(mock_requests_get):
+    issue_body = {"body": "Mock issue body for testing"}
+    mock_requests_get.return_value.json.return_value = issue_body
+
+    issue_form = IssueForm("testuser/testrepo", 123)
+    rendered_issue = issue_form.render()
+
+    assert isinstance(rendered_issue, dict)
+    assert rendered_issue == IssueTemplate(issue_body["body"]).to_dict()
