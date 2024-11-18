2a45e60910a2823038c5de13a60dd9d197939c5a
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-11-18 17:19:52 +0330

    added docstrings

--- pyaction/exceptions.py
+++ pyaction/exceptions.py
@@ -1,6 +1,10 @@
 class WorkflowParameterNotFound(Exception):
+    """Raised when a parameter for the workflow is not found."""
+
     pass
 
 
 class NotAnnotated(Exception):
+    """Raised when a function is not properly annotated."""
+
     pass
