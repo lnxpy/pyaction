3d7ca0ead094ed893fbedb19cb15f781bae74c94
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-11-18 17:24:44 +0330

    adding GITHUB API service url

--- pyaction/issues/connector.py
+++ pyaction/issues/connector.py
@@ -1,32 +1,31 @@
-from collections import OrderedDict
-
 import requests
 
-from pyaction.consts import GITHUB_BASE_URL
 from pyaction.issues.rendering import IssueTemplate
+
+GITHUB_BASE_URL = "https://api.github.com/"
 
 
 class IssueForm:
     def __init__(self, repository: str, number: int, token: str | None = None) -> None:
-        """initializer
+        """
+        initializer
 
         Args:
             repository (str): repository name in the form of username/repository
             number (int): issue number/ID
             token (str, optional): GITHUB_TOKEN token. Defaults to None.
         """
-
         self._token = token
         self.repository = repository
         self.number = number
 
     def render(self) -> dict[str, str]:
-        """renders the issue body
+        """
+        renders the issue body
 
         Returns:
             OrderedDict: the issue body in form of dictionary
         """
-
         headers = {"Accept": "application/vnd.github+json"}
 
         if self._token:
